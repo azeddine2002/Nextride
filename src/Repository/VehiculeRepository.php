a0158652d6f42fc3fcc6501c2e15b07b8236f93b
Azeddine amari <azeddineamari05@gmail.com>
2026-07-29 11:24:38 +0200

    feat: page d accueil avec recherche et filtres vehicules

--- src/Repository/VehiculeRepository.php
+++ src/Repository/VehiculeRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Vehicule;
+use App\Enum\CategorieVehicule;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,6 +15,35 @@ class VehiculeRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Vehicule::class);
+    }
+
+    /**
+     * @param CategorieVehicule[] $categories
+     *
+     * @return Vehicule[]
+     */
+    public function rechercher(?string $recherche, array $categories, ?float $prixMax): array
+    {
+        $qb = $this->createQueryBuilder('v')
+            ->andWhere('v.disponible = true')
+            ->orderBy('v.prixJour', 'ASC');
+
+        if ($recherche) {
+            $qb->andWhere('v.marque LIKE :recherche OR v.modele LIKE :recherche')
+                ->setParameter('recherche', '%'.$recherche.'%');
+        }
+
+        if ([] !== $categories) {
+            $qb->andWhere('v.categorie IN (:categories)')
+                ->setParameter('categories', $categories);
+        }
+
+        if (null !== $prixMax) {
+            $qb->andWhere('v.prixJour <= :prixMax')
+                ->setParameter('prixMax', $prixMax);
+        }
+
+        return $qb->getQuery()->getResult();
     }
 
     //    /**
